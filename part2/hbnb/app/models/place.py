303554a38354df945310082943116c1bc18646e5
C4lice <laura.aupetit002@gmail.com>
2025-06-18 14:53:27 +0200

    fix: latitude and longitude to place

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,12 +1,13 @@
 from __init__ import BaseModel
 
 class Place(BaseModel):
-    def __init__(self, owner_id, name, description='', city='', price=0):
+    def __init__(self, owner, name, description, latitude, longitude, price):
         super().__init__()
-        self.owner_id = owner_id      # id of the User who created the location
+        self.owner = owner      # id of the User who created the location
         self.name = name
         self.description = description
-        self.city = city
+        self.latitude = latitude
+        self.longitude = longitude
         self.price = price
-        self.amenity_ids = []         # list of Amenities ids
+        self.amenity = []         # list of Amenities ids
         self.reviews = []             # Review object list
