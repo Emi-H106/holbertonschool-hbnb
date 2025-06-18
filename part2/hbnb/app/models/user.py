7836abadcdeeeb74d4f05851a682cd388efe1ed6
Emi-H106 <emi.hatano@yahoo.com>
2025-06-18 09:53:08 +0200

    add base model

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,20 +1,13 @@
-import uuid
+from app.models.base_model import BaseModel
 
-class User:
+class User(BaseModel):
     def __init__(self, first_name, last_name, email, id=None):
-        self.id = id if id else str(uuid.uuid4())
+        super().__init__()
+        if id:
+            self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-
-    def update(self, data):
-        """Update user information based on the provided data."""
-        if 'first_name' in data:
-            self.first_name = data['first_name']
-        if 'last_name' in data:
-            self.last_name = data['last_name']
-        if 'email' in data:
-            self.email = data['email']
 
     def to_dict(self):
         """Convert the user object to a dictionary."""
@@ -22,6 +15,7 @@ class User:
                 'id': self.id,
                 'first_name': self.first_name,
                 'last_name': self.last_name,
-                'email': self.email
+                'email': self.email,
+                'created.at': self.created_at.isoformat(),
+                'updated_at': self.updated_at.isoformat()
         }
-
